1d5d7133a538b4c664ccdb436742673b467ded61
Nana A. Odeibea Amoah <31593244+Namoah1@users.noreply.github.com>
2021-05-27 13:37:24 -0500

    Add files via upload

---
+++ picScroll/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  picScroll
+//
+//  Created by Nana Adwoa Odeibea Amoah on 5/27/21.
+//  Copyright © 2021 Nana Adwoa Odeibea Amoah. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
